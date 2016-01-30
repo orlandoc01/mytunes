0ab118fe45362c2ac0fe1b67239bff8d2154d01b
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-29 18:10:25 -0800

    Implements all methods on SongQueue Class

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,35 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
-  }
+    this.on('add', function() {
+      if(this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.remove(this.at(0)); //could be a catostrophic bug
+      if(this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function() {
+      this.remove(this.at(0));
+
+    },this);
+
+  },
+
+  playFirst: function() {
+    if(this.length > 0) {
+      console.log(this.at(0));
+      this.at(0).play();
+    }
+  },
+  // remove: function() {
+  //   debugger;
+  //   return this.shift();
+  // }
 
 });
